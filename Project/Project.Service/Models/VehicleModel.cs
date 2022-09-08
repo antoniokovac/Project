3c7627e41a31676672a896de3b51c49f5bffd319
antoniokovac <antonio.kovac0000@gmail.com>
2022-09-08 20:52:55 +0200

    created VehicleMake and VehicleModel classes changed nullable property in PropertyGroup from "enable" to "disable"

---
+++ Project/Project.Service/Models/VehicleModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Models
+{
+    public class VehicleModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("MakeId")]
+        public int MakeId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Abrv { get; set; }
+
+        public int VehicleMakeId { get; set; }
+        public VehicleMake VehicleMake { get; set; }
+
+    }
+}
