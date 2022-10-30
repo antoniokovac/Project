f190a0636fdaffa7095d6aa072254a16c1615ef8
antoniokovac <antonio.kovac0000@gmail.com>
2022-10-30 20:27:17 +0100

    Created Project.Models Added VehicleMake and VeihcleModels Created Project.DAL Added VehicleDbContext Adeed initial migration

---
+++ Project/Project.DAL/Migrations/20221030192118_InitialMigration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.DAL.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VehicleMake",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Abrv = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleMake", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehicleModel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Abrv = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    VehicleMakeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VehicleModel_VehicleMake_VehicleMakeId",
+                        column: x => x.VehicleMakeId,
+                        principalTable: "VehicleMake",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleModel_VehicleMakeId",
+                table: "VehicleModel",
+                column: "VehicleMakeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehicleModel");
+
+            migrationBuilder.DropTable(
+                name: "VehicleMake");
+        }
+    }
+}
---
+++ Project/Project.Model/DatabaseModels/VehicleModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model.DatabaseModels
+{
+    public class VehicleModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Abrv { get; set; }
+
+        [Required]
+        public Guid VehicleMakeId { get; set; }
+
+        [ForeignKey("VehicleMakeId")]
+        public VehicleMake VehicleMake { get; set; }
+
+    }
+}
